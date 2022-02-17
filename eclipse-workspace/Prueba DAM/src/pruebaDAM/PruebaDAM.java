3f24a25a876946eaeeb4d25dbd6532fab0e293cb
ptrdck <94466380+ptrdck@users.noreply.github.com>
2022-02-17 17:58:34 +0100

    Update PruebaDAM.java

--- eclipse-workspace/Prueba DAM/src/pruebaDAM/PruebaDAM.java
+++ eclipse-workspace/Prueba DAM/src/pruebaDAM/PruebaDAM.java
@@ -13,7 +13,7 @@ public class PruebaDAM {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		System.out.println("comenzando");
+		System.out.println("comenzando desde github");
 
 	}
 
